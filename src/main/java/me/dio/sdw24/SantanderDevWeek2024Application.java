2dc223d8b2f963e76f346c150fbe577e76a17766
henriquecesarf <henriquecfdantas@gmail.com>
2024-03-25 21:31:54 -0300

    Initial commit

---
+++ src/main/java/me/dio/sdw24/SantanderDevWeek2024Application.java
@@ -0,0 +1,21 @@
+package me.dio.sdw24;
+
+import me.dio.sdw24.aplication.ListChampionsUseCase;
+import me.dio.sdw24.domain.ports.ChampionsRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SantanderDevWeek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2024Application.class, args);
+	}
+
+	@Bean
+	public ListChampionsUseCase provideListChampionsUseCase(ChampionsRepository repository){
+		return new ListChampionsUseCase(repository);
+	}
+
+}
