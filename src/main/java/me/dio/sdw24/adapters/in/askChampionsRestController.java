b0ffec9944448cc977f291f737dd356439b644e7
henriquecesarf <henriquecfdantas@gmail.com>
2024-03-28 16:43:28 -0300

    ajustes

--- src/main/java/me/dio/sdw24/adapters/in/askChampionsRestController.java
+++ src/main/java/me/dio/sdw24/adapters/in/askChampionsRestController.java
@@ -1,20 +1,20 @@
 package me.dio.sdw24.adapters.in;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import me.dio.sdw24.aplication.AskChampionsUseCase;
+import me.dio.sdw24.application.AskChampionUseCase;
 
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Campeões",  description = "Endpoints do domínio de campeões de LOL.")
 @RestController
 @RequestMapping("/champions")
-public record askChampionsRestController(AskChampionsUseCase useCase) {
+public record askChampionsRestController(AskChampionUseCase useCase) {
     @PostMapping("/{championId}/ask")
     public AskChampionRequestResponse askChampion(@PathVariable Long championId, @RequestBody AskChampionRequest request) {
         String answer = useCase.askChampion(championId, request.question());
         return new AskChampionRequestResponse(answer);
     }
 
-    public record AskChampionRequest(String question){};
-    public record AskChampionRequestResponse(String answer){};
+    public record AskChampionRequest(String question){}
+    public record AskChampionRequestResponse(String answer){}
 }
