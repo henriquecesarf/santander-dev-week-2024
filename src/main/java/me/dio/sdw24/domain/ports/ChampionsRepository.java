1f1e1806696104e5388232f33f3a88d1a05ee736
henriquecesarf <henriquecfdantas@gmail.com>
2024-03-28 15:39:05 -0300

    commit do terceiro dia do santander dev week criando api

--- src/main/java/me/dio/sdw24/domain/ports/ChampionsRepository.java
+++ src/main/java/me/dio/sdw24/domain/ports/ChampionsRepository.java
@@ -1,11 +1,13 @@
 package me.dio.sdw24.domain.ports;
 
-import me.dio.sdw24.domain.model.Champions;
+import me.dio.sdw24.domain.model.Champion;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ChampionsRepository {
-    List<Champions> findAll();
-    Optional<Champions> findById(Long id);
+
+    List<Champion> findAll();
+
+    Optional<Champion> findById(Long id);
 }
